e1d296953301083b24ccb3fc63ca3b5287266606
ali1129js <ali1129js@gmail.com>
2019-11-21 15:55:49 +0100

    main logic works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @Author: Ali
+ * @Date:   2019-11-21T14:46:30+01:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2019-11-21T15:54:56+01:00
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Player from "./components/Player";
+
+import "./App.css";
+
+const weapons = ["rock", "paper", "scissors"];
+
+class App extends Component {
+  state = {
+    playerOne: weapons[0],
+    playerTwo: weapons[1]
+  };
+  selectWinner = () => {
+    const { playerOne, playerTwo } = this.state;
+    if (playerOne === playerTwo) {
+      return "It's a tie";
+    } else if (
+      (playerOne === "rock" && playerTwo === "scissors") ||
+      (playerOne === "paper" && playerTwo === "rock") ||
+      (playerOne === "scissors" && playerTwo === "paper")
+    ) {
+      return "Player One is the Winner";
+    } else {
+      return "Player Two is the Winner";
+    }
+  };
+  setWeapon = weapon => {
+    this.setState({
+      playerOne: weapon
+    });
+  };
+  render() {
+    const { playerOne, playerTwo } = this.state;
+    return (
+      <div className="App">
+        <Player weapon={playerOne} />
+        <Player weapon={playerTwo} />
+        <button onClick={() => this.setWeapon("rock")}> rock </button>
+        <button onClick={() => this.setWeapon("paper")}> paper </button>
+        <button onClick={() => this.setWeapon("scissors")}> scissors </button>
+        <div className="calc"> {this.selectWinner()} </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,11 @@
+/**
+ * @Author: Ali
+ * @Date:   2019-11-21T14:52:09+01:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2019-11-21T15:18:10+01:00
+ */
+import React from "react";
+
+const Player = ({ weapon }) => <div className=""> {weapon}</div>;
+
+export default Player;
