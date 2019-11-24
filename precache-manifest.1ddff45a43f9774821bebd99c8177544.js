bf2d0a0893ec283c8a8032606bc9987cd2ba2074
ali1129js <ali1129js@gmail.com>
2019-11-24 06:32:03 +0100

    Updates

---
+++ precache-manifest.1ddff45a43f9774821bebd99c8177544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d09da81a11bb802f4fbaa3293365fe",
+    "url": "/rps-game/index.html"
+  },
+  {
+    "revision": "8eb66a74998196672e70",
+    "url": "/rps-game/static/css/main.f5a92f1d.chunk.css"
+  },
+  {
+    "revision": "1448e5ab4bdb74b404a7",
+    "url": "/rps-game/static/js/2.93b50bed.chunk.js"
+  },
+  {
+    "revision": "8eb66a74998196672e70",
+    "url": "/rps-game/static/js/main.def24f3a.chunk.js"
+  },
+  {
+    "revision": "d106a41a37b8b2b49e3d",
+    "url": "/rps-game/static/js/runtime-main.faec31bb.js"
+  }
+]);
