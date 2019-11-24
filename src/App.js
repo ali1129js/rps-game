8968cecde47513012a96716578efeb4adf145274
ali1129js <ali1129js@gmail.com>
2019-11-24 05:39:13 +0100

    fixed image import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: Ali
  * @Date:   2019-11-21T14:46:30+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-11-21T15:54:56+01:00
+ * @Last modified time: 2019-11-24T05:37:57+01:00
  */
 
 import React, { Component } from "react";
@@ -14,21 +14,37 @@ const weapons = ["rock", "paper", "scissors"];
 
 class App extends Component {
   state = {
-    playerOne: weapons[0],
-    playerTwo: weapons[1]
+    player: weapons[0],
+    computer: weapons[1]
+  };
+  startGame = () => {
+    let counter = 0;
+    let gameInterval = setInterval(() => {
+      counter++;
+      this.setState({
+        computer: weapons[Math.floor(Math.random() * weapons.length)],
+        winner: ""
+      });
+      if (counter > 5) {
+        clearInterval(gameInterval);
+        this.setState({
+          winner: this.selectWinner()
+        });
+      }
+    }, 200);
   };
   selectWinner = () => {
-    const { playerOne, playerTwo } = this.state;
-    if (playerOne === playerTwo) {
+    const { player, computer } = this.state;
+    if (player === computer) {
       return "It's a tie";
     } else if (
-      (playerOne === "rock" && playerTwo === "scissors") ||
-      (playerOne === "paper" && playerTwo === "rock") ||
-      (playerOne === "scissors" && playerTwo === "paper")
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
     ) {
-      return "Player One is the Winner";
+      return "Player is the Winner";
     } else {
-      return "Player Two is the Winner";
+      return "Computer is the Winner";
     }
   };
   setWeapon = weapon => {
@@ -37,15 +53,20 @@ class App extends Component {
     });
   };
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { player, computer, pscore, cscore } = this.state;
     return (
       <div className="App">
-        <Player weapon={playerOne} />
-        <Player weapon={playerTwo} />
+        <div className="names">Player:{pscore}</div>
+        <Player weapon={player} />
+        <div className="names">Computer:{cscore}</div>
+        <Player weapon={computer} />
         <button onClick={() => this.setWeapon("rock")}> rock </button>
         <button onClick={() => this.setWeapon("paper")}> paper </button>
         <button onClick={() => this.setWeapon("scissors")}> scissors </button>
         <div className="calc"> {this.selectWinner()} </div>
+        <button className="start" type="button" onClick={this.startGame}>
+          Start!
+        </button>
       </div>
     );
   }
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,10 +2,21 @@
  * @Author: Ali
  * @Date:   2019-11-21T14:52:09+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-11-21T15:18:10+01:00
+ * @Last modified time: 2019-11-24T05:30:02+01:00
  */
 import React from "react";
+import rock from "./rock.png";
+import scissors from "./scissors.png";
+import paper from "./paper.png";
 
-const Player = ({ weapon }) => <div className=""> {weapon}</div>;
+const Player = ({ weapon }) => (
+  <div className="player">
+    <img
+      className="player-imgage"
+      src={weapon === "rock" ? rock : weapon === "paper" ? paper : scissors}
+      alt="Rock Paper Scissors"
+    />
+  </div>
+);
 
 export default Player;
